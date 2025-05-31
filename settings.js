bc1114f338c274d2026a87094adde135cd2747a8
pemitha123 <193597823+pemitha123@users.noreply.github.com>
2025-05-31 14:39:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'FRwW3aJR#AkyHpsr-u-rRYnrs9BCtIY0fKMhEW7enI7NzVdzZXKo' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "අසිත" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "අසත" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
